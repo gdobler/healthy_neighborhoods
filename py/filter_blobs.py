806d447cc1fcfe5fe8aadd6c65440f4449980583
Greg Dobler <greg.dobler@gmail.com>
2017-05-08 16:48:29 -0400

    only convert to string once

--- py/filter_blobs.py
+++ py/filter_blobs.py
@@ -13,7 +13,7 @@ gdict = {}
 # -- read in blobs
 bname = os.path.join("..", "output","blobs_TLC00001.json")
 bdict = json.load(open(bname, "r"))
-tss   = sorted([int(i) for i in bdict.keys()])
+tss   = [str(i) for i in sorted([int(i) for i in bdict.keys()])]
 hdict = copy.copy(bdict)
 
 
@@ -22,20 +22,19 @@ hdict = copy.copy(bdict)
 #            if there is a blob in the next frame or previous frame within 
 #            the radius remove it and add this index to the pop dict
 for ts0, ts1, ts2 in zip(tss[:-2], tss[1:-1], tss[2:]):
-    sts = str(ts1)
-    bc1 = bdict[sts]
+    bc1 = bdict[ts1]
     nbc = len(bc1)
     if nbc == 0:
         continue
     else:
-        gdict[sts] = np.ones(nbc, dtype=bool)
-    bcc = bdict[str(ts0)] + bdict[str(ts2)]
+        gdict[ts1] = np.ones(nbc, dtype=bool)
+    bcc = bdict[ts0] + bdict[ts2]
 
     for ii in range(nbc):
         for jj in range(len(bcc)):
             dr2 = (bc1[ii][0] - bcc[jj][0])**2 + (bc1[ii][1] - bcc[jj][1])**2
             if dr2 <= rad2:
-                gdict[sts][ii] = False
+                gdict[ts1][ii] = False
                 break
 
 
